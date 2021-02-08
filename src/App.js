033058b478ef66eb1768415c7d78ffc6e4450266
ArmandoMota <armandomotagt@gmail.com>
2021-02-08 11:37:51 -0800

    Displaying raw data

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
+// const data = require('./data.js');
+import data from './data';
 
 const App = () => (
   <div className="app">
@@ -10,6 +12,24 @@ const App = () => (
     <p>
       Welcome to the app!
     </p>
+    <table>
+      <thead>
+        <tr>
+          <th>Airline</th>
+          <th>Source</th>
+          <th>Destination</th>
+        </tr>
+      </thead>
+      <tbody>
+        {data.routes.map(route => (
+          <tr>
+            <td>{route.airline}</td>
+            <td>{route.src}</td>
+            <td>{route.dest}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
   </section>
 </div>
 )
